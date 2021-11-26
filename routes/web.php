0b2395be2dff9ee9650b8d272c198e4f8755aabc
Maksym Solomianyi <maxssolom@gmail.com>
2021-11-26 19:33:07 +0200

    Add routes for authentication

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,13 @@ Route::get('/', '\App\Http\Controllers\MainController@index')->name('home');
 Route::get('/books', '\App\Http\Controllers\BookController@index')->name('books');
 Route::post('/books', '\App\Http\Controllers\BookController@index')->name('books');
 Route::get('/categories', '\App\Http\Controllers\CategoryController@index')->name('categories');
+Route::name('auth.')->group(function() {
+    Route::get('/login', '\App\Http\Controllers\LoginController@index')->name('login');
+    Route::post('/login', '\App\Http\Controllers\LoginController@login');
+    Route::get('/logout', '\App\Http\Controllers\LoginController@logout')->name('logout');
+    Route::get(
+        '/registration',
+        '\App\Http\Controllers\RegistrationController@index'
+    )->name('registration');
+    Route::post('/registration', '\App\Http\Controllers\RegistrationController@save');
+});
