3250d2385336ff857f3f624c0a47a7547a41f464
Maksym Solomianyi <maxssolom@gmail.com>
2021-12-31 13:03:55 +0200

    Add BookController.php file.

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Filters\BookFilter;
+use App\Http\Requests\AdminBookRequest;
+use App\Http\Requests\CreateAuthorRequest;
+use App\Http\Requests\CreateBookRequest;
+use App\Http\Requests\UpdateBookRequest;
+use App\Models\Author;
 use App\Models\Books;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class BookController extends Controller
 {
@@ -25,5 +31,53 @@ class BookController extends Controller
             return $query->where('book_id', $id);
         })->exists();
         return view('books.show', ['book' => $book, 'favorite' => $favorite]);
+    }
+
+    public function update(UpdateBookRequest $request, $id)
+    {
+        $book = Books::findOrFail($id);
+        $book->fill([
+            'title' => $request->title,
+            'description' => $request->description,
+            'path_file' => $request->bookFile !== null ? $request->bookFile->getClientOriginalName() : $book->path_file,
+        ]);
+        $book->save();
+        if ($request->bookFile !== null) {
+            $request->bookFile->storeAs('Books', $book->path_file);
+        }
+        $book->authors()->detach();
+        $book->authors()->attach($request->authors);
+        return redirect(route('admin.book.update', $book));
+    }
+
+    public function edit($id)
+    {
+        $book = Books::with('authors')->findOrFail($id);
+        $authors = Author::all();
+        return view('admin.book-update', ['book' => $book, 'authors' => $authors]);
+    }
+
+    public function create()
+    {
+        $authors = Author::all();
+        return view('admin.book-create', ['authors' => $authors]);
+    }
+
+    public function store(CreateBookRequest $request)
+    {
+        $book = Books::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'path_file' => $request->bookFile->getClientOriginalName(),
+        ]);
+        Storage::disk('local')->put($book->path_file, 'Books');
+        $book->authors()->attach($request->authors);
+        return redirect(route('admin.dashboard'));
+    }
+
+    public function destroy(Request $request)
+    {
+        $book = Books::findOrFail($request->post('id'))->delete();
+        return view('admin.books-ajax', ['books' => Books::all()])->render();
     }
 }
